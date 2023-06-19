6c230bf323c8e713f6b1f7f8e1f21b670b5f2b2b
can <canbolukbas98@gmail.com>
2023-06-19 14:51:08 +0300

    Don't show events that are ended

--- findlatinnights/events/views.py
+++ findlatinnights/events/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.utils import timezone
 from findlatinnights.events.models import Event
 
 
@@ -7,3 +8,8 @@ class EventListView(ListView):
     template_name = 'event_list.html'
     context_object_name = 'events'
     ordering = 'start_datetime'
+
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        now = timezone.now()
+        return queryset.filter(end_datetime__gte=now)
