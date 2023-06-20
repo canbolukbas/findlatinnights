27a8051416857850ec1937112ef9fa3fd6622968
can <canbolukbas98@gmail.com>
2023-06-20 17:57:29 +0300

    Alter event image as null & blank True

--- findlatinnights/events/models.py
+++ findlatinnights/events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='event_images')
+    image = models.ImageField(upload_to='event_images', blank=True, null=True)
     venue = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     does_play_bachata = models.BooleanField()
